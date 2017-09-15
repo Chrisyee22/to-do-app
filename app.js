ed3ef4c45caccf74fa708f8ace439f9520ef34d4
Christopher Yee <chris.anthony.yee@gmail.com>
2017-09-15 15:10:23 -0700

    todo

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ function onReady(){
   const addToDoForm = document.getElementById("addToDoForm");
   const newToDoText = document.getElementById("newToDoText");
   const toDoList = document.getElementById("toDoList");
-
+  const deleteToDo= document.getElementById('deleteToDo');
 
   addToDoForm.addEventListener("submit", (event) => {
     event.preventDefault();
     //get the text
     let title = newToDoText.value;
-    console.log(title);
+
 
     //create a new li
     let newLi = document.createElement('li');
@@ -31,9 +31,47 @@ function onReady(){
     //empty input
     newToDoText.value="";
 
+
   });
 
+    deleteToDo.addEventListener('click' , deleteList );
+
+
+
 }
+
+function deleteList(event){
+  event.preventDefault();
+//Goal:delete checked items
+// When a button is clicked go over all li elements
+const deleteToDoForm = document.querySelector('#toDoList');
+  // event.preventDefault();
+const listItems = deleteToDoForm.querySelectorAll ('li');
+  // loop over all elements
+  for (var i=0 ; i < listItems.length ; i++){
+    let currentLi = listItems[i];
+    let checkbox= currentLi.querySelector('input[type="checkbox"]');
+
+    //check for li elements
+  //check to see if box is checked
+
+        if(checkbox.checked ===true){
+          //if box is checked identifiy element
+         //delete elements that are identified as checked
+
+         deleteToDoForm.removeChild(currentLi);
+        }
+
+
+
+    }
+
+
+}
+
+
+
+
 
 
 window.onload = function(){
